danyx23/pi-mono

feat(oauth): add codex login method chooser

packages/ai/src/cli.ts
@@ -2,7 +2,7 @@
 
 import { existsSync, readFileSync, writeFileSync } from "fs";
 import { createInterface } from "readline";
-import { getOAuthProvider, getOAuthProviders } from "./utils/oauth/index.js";
+import { getOAuthCredentialStorageId, getOAuthProvider, getOAuthProviders } from "./utils/oauth/index.js";
 import type { OAuthCredentials, OAuthProviderId } from "./utils/oauth/types.js";
 
 const AUTH_FILE = "auth.json";
@@ -49,7 +49,7 @@ async function login(providerId: OAuthProviderId): Promise<void> {
 		});
 
 		const auth = loadAuth();
-		auth[providerId] = { type: "oauth", ...credentials };
+		auth[getOAuthCredentialStorageId(providerId)] = { type: "oauth", ...credentials };
 		saveAuth(auth);
 
 		console.log(`\nCredentials saved to ${AUTH_FILE}`);

packages/ai/src/utils/oauth/index.ts
@@ -24,7 +24,13 @@ export { antigravityOAuthProvider, loginAntigravity, refreshAntigravityToken } f
 // Google Gemini CLI
 export { geminiCliOAuthProvider, loginGeminiCli, refreshGoogleCloudToken } from "./google-gemini-cli.js";
 // OpenAI Codex (ChatGPT OAuth)
-export { loginOpenAICodex, openaiCodexOAuthProvider, refreshOpenAICodexToken } from "./openai-codex.js";
+export {
+	loginOpenAICodex,
+	loginOpenAICodexDeviceCode,
+	openaiCodexDeviceCodeOAuthProvider,
+	openaiCodexOAuthProvider,
+	refreshOpenAICodexToken,
+} from "./openai-codex.js";
 
 export * from "./types.js";
 
@@ -36,7 +42,7 @@ import { anthropicOAuthProvider } from "./anthropic.js";
 import { githubCopilotOAuthProvider } from "./github-copilot.js";
 import { antigravityOAuthProvider } from "./google-antigravity.js";
 import { geminiCliOAuthProvider } from "./google-gemini-cli.js";
-import { openaiCodexOAuthProvider } from "./openai-codex.js";
+import { openaiCodexDeviceCodeOAuthProvider, openaiCodexOAuthProvider } from "./openai-codex.js";
 import type { OAuthCredentials, OAuthProviderId, OAuthProviderInfo, OAuthProviderInterface } from "./types.js";
 
 const BUILT_IN_OAUTH_PROVIDERS: OAuthProviderInterface[] = [
@@ -45,6 +51,7 @@ const BUILT_IN_OAUTH_PROVIDERS: OAuthProviderInterface[] = [
 	geminiCliOAuthProvider,
 	antigravityOAuthProvider,
 	openaiCodexOAuthProvider,
+	openaiCodexDeviceCodeOAuthProvider,
 ];
 
 const oauthProviderRegistry = new Map<string, OAuthProviderInterface>(
@@ -56,6 +63,14 @@ const oauthProviderRegistry = new Map<string, OAuthProviderInterface>(
  */
 export function getOAuthProvider(id: OAuthProviderId): OAuthProviderInterface | undefined {
 	return oauthProviderRegistry.get(id);
+}
+
+/**
+ * Resolve the credential storage ID for a provider.
+ * Alias providers can share stored credentials with a canonical provider ID.
+ */
+export function getOAuthCredentialStorageId(id: OAuthProviderId): OAuthProviderId {
+	return getOAuthProvider(id)?.credentialsProviderId ?? id;
 }
 
 /**
@@ -143,7 +158,8 @@ export async function getOAuthApiKey(
 		throw new Error(`Unknown OAuth provider: ${providerId}`);
 	}
 
-	let creds = credentials[providerId];
+	const credentialsProviderId = getOAuthCredentialStorageId(providerId);
+	let creds = credentials[credentialsProviderId];
 	if (!creds) {
 		return null;
 	}

packages/coding-agent/src/modes/interactive/components/oauth-selector.ts
@@ -9,6 +9,7 @@ import { DynamicBorder } from "./dynamic-border.js";
  * Component that renders an OAuth provider selector
  */
 export class OAuthSelectorComponent extends Container {
+	private titleContainer: Container;
 	private listContainer: Container;
 	private allProviders: OAuthProviderInterface[] = [];
 	private selectedIndex: number = 0;
@@ -16,6 +17,7 @@ export class OAuthSelectorComponent extends Container {
 	private authStorage: AuthStorage;
 	private onSelectCallback: (providerId: string) => void;
 	private onCancelCallback: () => void;
+	private loginMethodParentProviderId: string | null = null;
 
 	constructor(
 		mode: "login" | "logout",
@@ -38,8 +40,8 @@ export class OAuthSelectorComponent extends Container {
 		this.addChild(new Spacer(1));
 
 		// Add title
-		const title = mode === "login" ? "Select provider to login:" : "Select provider to logout:";
-		this.addChild(new TruncatedText(theme.bold(title)));
+		this.titleContainer = new Container();
+		this.addChild(this.titleContainer);
 		this.addChild(new Spacer(1));
 
 		// Create list container
@@ -59,63 +61,133 @@ export class OAuthSelectorComponent extends Container {
 		this.allProviders = getOAuthProviders();
 	}
 
+	private getParentProvider(): OAuthProviderInterface | undefined {
+		if (!this.loginMethodParentProviderId) return undefined;
+		return this.allProviders.find((provider) => provider.id === this.loginMethodParentProviderId);
+	}
+
+	private getVisibleProviders(): OAuthProviderInterface[] {
+		if (this.mode !== "login" || !this.loginMethodParentProviderId) {
+			return this.allProviders.filter((provider) => !provider.parentProviderId);
+		}
+
+		const parentProvider = this.getParentProvider();
+		if (!parentProvider) {
+			return this.allProviders.filter((provider) => !provider.parentProviderId);
+		}
+
+		const alternateProviders = this.allProviders.filter(
+			(provider) => provider.parentProviderId === this.loginMethodParentProviderId,
+		);
+		return [parentProvider, ...alternateProviders];
+	}
+
+	private getTitle(): string {
+		if (this.mode === "logout") {
+			return "Select provider to logout:";
+		}
+
+		const parentProvider = this.getParentProvider();
+		if (parentProvider) {
+			return `Select login method for ${parentProvider.name}:`;
+		}
+
+		return "Select provider to login:";
+	}
+
+	private getProviderLabel(provider: OAuthProviderInterface): string {
+		if (this.mode === "login" && this.loginMethodParentProviderId) {
+			return provider.loginOptionLabel ?? provider.name;
+		}
+		return provider.name;
+	}
+
 	private updateList(): void {
+		this.titleContainer.clear();
+		this.titleContainer.addChild(new TruncatedText(theme.bold(this.getTitle())));
 		this.listContainer.clear();
 
-		for (let i = 0; i < this.allProviders.length; i++) {
-			const provider = this.allProviders[i];
+		const visibleProviders = this.getVisibleProviders();
+		if (this.selectedIndex >= visibleProviders.length) {
+			this.selectedIndex = Math.max(0, visibleProviders.length - 1);
+		}
+
+		for (let i = 0; i < visibleProviders.length; i++) {
+			const provider = visibleProviders[i];
 			if (!provider) continue;
 
 			const isSelected = i === this.selectedIndex;
-
-			// Check if user is logged in for this provider
+			const showStatusIndicator = !this.loginMethodParentProviderId;
 			const credentials = this.authStorage.get(provider.id);
 			const isLoggedIn = credentials?.type === "oauth";
-			const statusIndicator = isLoggedIn ? theme.fg("success", " ✓ logged in") : "";
+			const statusIndicator = showStatusIndicator && isLoggedIn ? theme.fg("success", " ✓ logged in") : "";
+			const label = this.getProviderLabel(provider);
 
 			let line = "";
 			if (isSelected) {
 				const prefix = theme.fg("accent", "→ ");
-				const text = theme.fg("accent", provider.name);
+				const text = theme.fg("accent", label);
 				line = prefix + text + statusIndicator;
 			} else {
-				const text = `  ${provider.name}`;
-				line = text + statusIndicator;
+				line = `  ${label}${statusIndicator}`;
 			}
 
 			this.listContainer.addChild(new TruncatedText(line, 0, 0));
 		}
 
-		// Show "no providers" if empty
-		if (this.allProviders.length === 0) {
+		if (visibleProviders.length === 0) {
 			const message =
 				this.mode === "login" ? "No OAuth providers available" : "No OAuth providers logged in. Use /login first.";
 			this.listContainer.addChild(new TruncatedText(theme.fg("muted", `  ${message}`), 0, 0));
 		}
 	}
 
+	private openLoginMethodSelector(provider: OAuthProviderInterface): void {
+		this.loginMethodParentProviderId = provider.id;
+		this.selectedIndex = 0;
+		this.updateList();
+	}
+
+	private closeLoginMethodSelector(): boolean {
+		if (!this.loginMethodParentProviderId) {
+			return false;
+		}
+
+		this.loginMethodParentProviderId = null;
+		this.selectedIndex = 0;
+		this.updateList();
+		return true;
+	}
+
 	handleInput(keyData: string): void {
 		const kb = getEditorKeybindings();
-		// Up arrow
+		const visibleProviders = this.getVisibleProviders();
+
 		if (kb.matches(keyData, "selectUp")) {
 			this.selectedIndex = Math.max(0, this.selectedIndex - 1);
 			this.updateList();
-		}
-		// Down arrow
-		else if (kb.matches(keyData, "selectDown")) {
-			this.selectedIndex = Math.min(this.allProviders.length - 1, this.selectedIndex + 1);
+		} else if (kb.matches(keyData, "selectDown")) {
+			this.selectedIndex = Math.min(visibleProviders.length - 1, this.selectedIndex + 1);
 			this.updateList();
-		}
-		// Enter
-		else if (kb.matches(keyData, "selectConfirm")) {
-			const selectedProvider = this.allProviders[this.selectedIndex];
-			if (selectedProvider) {
-				this.onSelectCallback(selectedProvider.id);
+		} else if (kb.matches(keyData, "selectConfirm")) {
+			const selectedProvider = visibleProviders[this.selectedIndex];
+			if (!selectedProvider) return;
+
+			const hasAlternateLoginMethods =
+				this.mode === "login" &&
+				!this.loginMethodParentProviderId &&
+				this.allProviders.some((provider) => provider.parentProviderId === selectedProvider.id);
+
+			if (hasAlternateLoginMethods) {
+				this.openLoginMethodSelector(selectedProvider);
+				return;
 			}
-		}
-		// Escape or Ctrl+C
-		else if (kb.matches(keyData, "selectCancel")) {
-			this.onCancelCallback();
+
+			this.onSelectCallback(selectedProvider.id);
+		} else if (kb.matches(keyData, "selectCancel")) {
+			if (!this.closeLoginMethodSelector()) {
+				this.onCancelCallback();
+			}
 		}
 	}
 }

earendil-works/pi

packages/ai/src/cli.ts
@@ -2,7 +2,7 @@
 
 import { createInterface } from "node:readline";
 import { existsSync, readFileSync, writeFileSync } from "fs";
-import { getOAuthProvider, getOAuthProviders } from "./utils/oauth/index.ts";
+import { getOAuthCredentialStorageId, getOAuthProvider, getOAuthProviders } from "./utils/oauth/index.ts";
 import type { OAuthCredentials, OAuthProviderId } from "./utils/oauth/types.ts";
 
 const AUTH_FILE = "auth.json";
@@ -49,7 +49,7 @@ async function login(providerId: OAuthProviderId): Promise<void> {
 		});
 
 		const auth = loadAuth();
-		auth[providerId] = { type: "oauth", ...credentials };
+		auth[getOAuthCredentialStorageId(providerId)] = { type: "oauth", ...credentials };
 		saveAuth(auth);
 
 		console.log(`\nCredentials saved to ${AUTH_FILE}`);

packages/ai/src/utils/oauth/index.ts
@@ -18,7 +18,13 @@ export {
 	refreshGitHubCopilotToken,
 } from "./github-copilot.ts";
 // OpenAI Codex (ChatGPT OAuth)
-export { loginOpenAICodex, openaiCodexOAuthProvider, refreshOpenAICodexToken } from "./openai-codex.ts";
+export {
+	loginOpenAICodex,
+	loginOpenAICodexDeviceCode,
+	openaiCodexDeviceCodeOAuthProvider,
+	openaiCodexOAuthProvider,
+	refreshOpenAICodexToken,
+} from "./openai-codex.ts";
 
 export * from "./types.ts";
 
@@ -28,13 +34,14 @@ export * from "./types.ts";
 
 import { anthropicOAuthProvider } from "./anthropic.ts";
 import { githubCopilotOAuthProvider } from "./github-copilot.ts";
-import { openaiCodexOAuthProvider } from "./openai-codex.ts";
+import { openaiCodexDeviceCodeOAuthProvider, openaiCodexOAuthProvider } from "./openai-codex.ts";
 import type { OAuthCredentials, OAuthProviderId, OAuthProviderInfo, OAuthProviderInterface } from "./types.ts";
 
 const BUILT_IN_OAUTH_PROVIDERS: OAuthProviderInterface[] = [
 	anthropicOAuthProvider,
 	githubCopilotOAuthProvider,
 	openaiCodexOAuthProvider,
+	openaiCodexDeviceCodeOAuthProvider,
 ];
 
 const oauthProviderRegistry = new Map<string, OAuthProviderInterface>(
@@ -46,6 +53,14 @@ const oauthProviderRegistry = new Map<string, OAuthProviderInterface>(
  */
 export function getOAuthProvider(id: OAuthProviderId): OAuthProviderInterface | undefined {
 	return oauthProviderRegistry.get(id);
+}
+
+/**
+ * Resolve the credential storage ID for a provider.
+ * Alias providers can share stored credentials with a canonical provider ID.
+ */
+export function getOAuthCredentialStorageId(id: OAuthProviderId): OAuthProviderId {
+	return getOAuthProvider(id)?.credentialsProviderId ?? id;
 }
 
 /**
@@ -133,7 +148,8 @@ export async function getOAuthApiKey(
 		throw new Error(`Unknown OAuth provider: ${providerId}`);
 	}
 
-	let creds = credentials[providerId];
+	const credentialsProviderId = getOAuthCredentialStorageId(providerId);
+	let creds = credentials[credentialsProviderId];
 	if (!creds) {
 		return null;
 	}

packages/coding-agent/src/modes/interactive/components/oauth-selector.ts
@@ -15,12 +15,16 @@ export type AuthSelectorProvider = {
 	id: string;
 	name: string;
 	authType: "oauth" | "api_key";
+	credentialsProviderId?: string;
+	parentProviderId?: string;
+	loginOptionLabel?: string;
 };
 
 /**
  * Component that renders an auth provider selector
  */
 export class OAuthSelectorComponent extends Container implements Focusable {
+	private titleContainer: Container;
 	private searchInput: Input;
 
 	// Focusable implementation - propagate to search input for IME cursor positioning
@@ -42,6 +46,7 @@ export class OAuthSelectorComponent extends Container implements Focusable {
 	private getAuthStatus: (providerId: string) => AuthStatus;
 	private onSelectCallback: (providerId: string) => void;
 	private onCancelCallback: () => void;
+	private loginMethodParentProviderId: string | null = null;
 
 	constructor(
 		mode: "login" | "logout",
@@ -66,16 +71,13 @@ export class OAuthSelectorComponent extends Container implements Focusable {
 		this.addChild(new Spacer(1));
 
 		// Add title
-		const title = mode === "login" ? "Select provider to configure:" : "Select provider to logout:";
-		this.addChild(new TruncatedText(theme.fg("accent", theme.bold(title)), 1, 0));
+		this.titleContainer = new Container();
+		this.addChild(this.titleContainer);
 		this.addChild(new Spacer(1));
 
 		this.searchInput = new Input();
 		this.searchInput.onSubmit = () => {
-			const selectedProvider = this.filteredProviders[this.selectedIndex];
-			if (selectedProvider) {
-				this.onSelectCallback(selectedProvider.id);
-			}
+			this.selectCurrentProvider();
 		};
 		this.addChild(this.searchInput);
 		this.addChild(new Spacer(1));
@@ -93,15 +95,63 @@ export class OAuthSelectorComponent extends Container implements Focusable {
 		this.filterProviders("");
 	}
 
+	private getParentProvider(): AuthSelectorProvider | undefined {
+		if (!this.loginMethodParentProviderId) return undefined;
+		return this.allProviders.find((provider) => provider.id === this.loginMethodParentProviderId);
+	}
+
+	private getSelectableProviders(): AuthSelectorProvider[] {
+		if (this.mode !== "login" || !this.loginMethodParentProviderId) {
+			return this.allProviders.filter((provider) => !provider.parentProviderId);
+		}
+
+		const parentProvider = this.getParentProvider();
+		if (!parentProvider) {
+			return this.allProviders.filter((provider) => !provider.parentProviderId);
+		}
+
+		const alternateProviders = this.allProviders.filter(
+			(provider) => provider.parentProviderId === this.loginMethodParentProviderId,
+		);
+		return [parentProvider, ...alternateProviders];
+	}
+
 	private filterProviders(query: string): void {
+		const selectableProviders = this.getSelectableProviders();
 		this.filteredProviders = query
-			? fuzzyFilter(this.allProviders, query, (provider) => `${provider.name} ${provider.id} ${provider.authType}`)
-			: this.allProviders;
+			? fuzzyFilter(
+					selectableProviders,
+					query,
+					(provider) => `${provider.name} ${provider.id} ${provider.authType} ${provider.loginOptionLabel ?? ""}`,
+				)
+			: selectableProviders;
 		this.selectedIndex = Math.max(0, Math.min(this.selectedIndex, Math.max(0, this.filteredProviders.length - 1)));
 		this.updateList();
 	}
 
+	private getTitle(): string {
+		if (this.mode === "logout") {
+			return "Select provider to logout:";
+		}
+
+		const parentProvider = this.getParentProvider();
+		if (parentProvider) {
+			return `Select login method for ${parentProvider.name}:`;
+		}
+
+		return "Select provider to configure:";
+	}
+
+	private getProviderLabel(provider: AuthSelectorProvider): string {
+		if (this.mode === "login" && this.loginMethodParentProviderId) {
+			return provider.loginOptionLabel ?? provider.name;
+		}
+		return provider.name;
+	}
+
 	private updateList(): void {
+		this.titleContainer.clear();
+		this.titleContainer.addChild(new TruncatedText(theme.fg("accent", theme.bold(this.getTitle())), 1, 0));
 		this.listContainer.clear();
 
 		const maxVisible = 8;
@@ -116,15 +166,16 @@ export class OAuthSelectorComponent extends Container implements Focusable {
 			if (!provider) continue;
 
 			const isSelected = i === this.selectedIndex;
+			const statusIndicator = this.loginMethodParentProviderId ? "" : this.formatStatusIndicator(provider);
+			const label = this.getProviderLabel(provider);
 
-			const statusIndicator = this.formatStatusIndicator(provider);
 			let line = "";
 			if (isSelected) {
 				const prefix = theme.fg("accent", "→ ");
-				const text = theme.fg("accent", provider.name);
+				const text = theme.fg("accent", label);
 				line = prefix + text + statusIndicator;
 			} else {
-				const text = `  ${theme.fg("text", provider.name)}`;
+				const text = `  ${theme.fg("text", label)}`;
 				line = text + statusIndicator;
 			}
 
@@ -174,6 +225,40 @@ export class OAuthSelectorComponent extends Container implements Focusable {
 		}
 	}
 
+	private openLoginMethodSelector(provider: AuthSelectorProvider): void {
+		this.loginMethodParentProviderId = provider.id;
+		this.selectedIndex = 0;
+		this.filterProviders(this.searchInput.getValue());
+	}
+
+	private closeLoginMethodSelector(): boolean {
+		if (!this.loginMethodParentProviderId) {
+			return false;
+		}
+
+		this.loginMethodParentProviderId = null;
+		this.selectedIndex = 0;
+		this.filterProviders(this.searchInput.getValue());
+		return true;
+	}
+
+	private selectCurrentProvider(): void {
+		const selectedProvider = this.filteredProviders[this.selectedIndex];
+		if (!selectedProvider) return;
+
+		const hasAlternateLoginMethods =
+			this.mode === "login" &&
+			!this.loginMethodParentProviderId &&
+			this.allProviders.some((provider) => provider.parentProviderId === selectedProvider.id);
+
+		if (hasAlternateLoginMethods) {
+			this.openLoginMethodSelector(selectedProvider);
+			return;
+		}
+
+		this.onSelectCallback(selectedProvider.id);
+	}
+
 	handleInput(keyData: string): void {
 		const kb = getKeybindings();
 		// Up arrow
@@ -190,14 +275,13 @@ export class OAuthSelectorComponent extends Container implements Focusable {
 		}
 		// Enter
 		else if (kb.matches(keyData, "tui.select.confirm")) {
-			const selectedProvider = this.filteredProviders[this.selectedIndex];
-			if (selectedProvider) {
-				this.onSelectCallback(selectedProvider.id);
-			}
+			this.selectCurrentProvider();
 		}
 		// Escape or Ctrl+C
 		else if (kb.matches(keyData, "tui.select.cancel")) {
-			this.onCancelCallback();
+			if (!this.closeLoginMethodSelector()) {
+				this.onCancelCallback();
+			}
 		}
 		// Pass everything else to search input
 		else {