weihao-m/pi-mono

feat(pi-ai): add stream idle timeout watchdog utility
Add withStreamIdleTimeout() async generator wrapper that detects
hung streaming connections. Races each iterator.next() against a
setTimeout. When no chunk arrives within the configured threshold,
throws StreamIdleTimeoutError.

Also adds streamIdleTimeoutMs to StreamOptions so consumers can
configure the timeout per request.

packages/ai/src/index.ts
@@ -31,5 +31,6 @@ export type {
 	OAuthProviderInterface,
 } from "./utils/oauth/types.js";
 export * from "./utils/overflow.js";
+export * from "./utils/stream-idle-timeout.js";
 export * from "./utils/typebox-helpers.js";
 export * from "./utils/validation.js";

earendil-works/pi

packages/ai/src/index.ts
@@ -43,5 +43,6 @@ export type {
 	OAuthSelectPrompt,
 } from "./utils/oauth/types.ts";
 export * from "./utils/overflow.ts";
+export * from "./utils/stream-idle-timeout.ts";
 export * from "./utils/typebox-helpers.ts";
 export * from "./utils/validation.ts";