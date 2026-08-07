mupt-ai/pi-mono

feat(coding-agent): add steppable session protocol

packages/coding-agent/src/main.ts
@@ -41,7 +41,7 @@ import { SessionManager } from "./core/session-manager.js";
 import { SettingsManager } from "./core/settings-manager.js";
 import { printTimings, resetTimings, time } from "./core/timings.js";
 import { runMigrations, showDeprecationWarnings } from "./migrations.js";
-import { InteractiveMode, runPrintMode, runRpcMode } from "./modes/index.js";
+import { InteractiveMode, runPrintMode, runRpcMode, runSteppableRpcMode } from "./modes/index.js";
 import { ExtensionSelectorComponent } from "./modes/interactive/components/extension-selector.js";
 import { initTheme, stopThemeWatcher } from "./modes/interactive/theme/theme.js";
 import { handleConfigCommand, handlePackageCommand } from "./package-manager-cli.js";
@@ -93,11 +93,11 @@ function isTruthyEnvFlag(value: string | undefined): boolean {
 	return value === "1" || value.toLowerCase() === "true" || value.toLowerCase() === "yes";
 }
 
-type AppMode = "interactive" | "print" | "json" | "rpc";
+type AppMode = "interactive" | "print" | "json" | "rpc" | "steppable-rpc";
 
 function resolveAppMode(parsed: Args, stdinIsTTY: boolean): AppMode {
-	if (parsed.mode === "rpc") {
-		return "rpc";
+	if (parsed.mode === "rpc" || parsed.mode === "steppable-rpc") {
+		return parsed.mode;
 	}
 	if (parsed.mode === "json") {
 		return "json";
@@ -108,7 +108,7 @@ function resolveAppMode(parsed: Args, stdinIsTTY: boolean): AppMode {
 	return "interactive";
 }
 
-function toPrintOutputMode(appMode: AppMode): Exclude<Mode, "rpc"> {
+function toPrintOutputMode(appMode: AppMode): Exclude<Mode, "rpc" | "steppable-rpc"> {
 	return appMode === "json" ? "json" : "text";
 }
 
@@ -472,8 +472,8 @@ export async function main(args: string[], options?: MainOptions) {
 		process.exit(0);
 	}
 
-	if (parsed.mode === "rpc" && parsed.fileArgs.length > 0) {
-		console.error(chalk.red("Error: @file arguments are not supported in RPC mode"));
+	if ((parsed.mode === "rpc" || parsed.mode === "steppable-rpc") && parsed.fileArgs.length > 0) {
+		console.error(chalk.red("Error: @file arguments are not supported in RPC modes"));
 		process.exit(1);
 	}
 
@@ -498,7 +498,10 @@ export async function main(args: string[], options?: MainOptions) {
 		parsed.sessionDir ??
 		(envSessionDir ? expandTildePath(envSessionDir) : undefined) ??
 		startupSettingsManager.getSessionDir();
-	let sessionManager = await createSessionManager(parsed, cwd, sessionDir, startupSettingsManager);
+	let sessionManager =
+		appMode === "steppable-rpc"
+			? SessionManager.inMemory(cwd)
+			: await createSessionManager(parsed, cwd, sessionDir, startupSettingsManager);
 	const missingSessionCwdIssue = getMissingSessionCwdIssue(sessionManager, cwd);
 	if (missingSessionCwdIssue) {
 		if (appMode === "interactive") {
@@ -635,9 +638,9 @@ export async function main(args: string[], options?: MainOptions) {
 		process.exit(0);
 	}
 
-	// Read piped stdin content (if any) - skip for RPC mode which uses stdin for JSON-RPC
+	// Read piped stdin content (if any) - skip for RPC modes which use stdin for JSON-RPC
 	let stdinContent: string | undefined;
-	if (appMode !== "rpc") {
+	if (appMode !== "rpc" && appMode !== "steppable-rpc") {
 		stdinContent = await readPipedStdin();
 		if (stdinContent !== undefined && appMode === "interactive") {
 			appMode = "print";
@@ -681,6 +684,9 @@ export async function main(args: string[], options?: MainOptions) {
 	if (appMode === "rpc") {
 		printTimings();
 		await runRpcMode(runtime);
+	} else if (appMode === "steppable-rpc") {
+		printTimings();
+		await runSteppableRpcMode(runtime);
 	} else if (appMode === "interactive") {
 		if (scopedModels.length > 0 && (parsed.verbose || !settingsManager.getQuietStartup())) {
 			const modelList = scopedModels

packages/coding-agent/src/modes/steppable-rpc/steppable-rpc-mode.ts
@@ -1,0 +1,100 @@
+/**
+ * Steppable RPC mode: JSONL request/response protocol for durable drivers.
+ *
+ * Unlike normal RPC mode, this does not stream agent events on stdout. Events
+ * are returned inside advance responses so the durable driver can persist them
+ * transactionally with the snapshot and next action.
+ */
+
+import type { AgentSessionRuntime } from "../../core/agent-session-runtime.js";
+import { takeOverStdout, writeRawStdout } from "../../core/output-guard.js";
+import { killTrackedDetachedChildren } from "../../utils/shell.js";
+import { attachJsonlLineReader, serializeJsonLine } from "../rpc/jsonl.js";
+import type { SteppableRpcCommand, SteppableRpcResponse } from "./steppable-rpc-types.js";
+
+export type { SteppableRpcCommand, SteppableRpcResponse } from "./steppable-rpc-types.js";
+
+export async function runSteppableRpcMode(runtimeHost: AgentSessionRuntime): Promise<never> {
+	takeOverStdout();
+	const session = runtimeHost.session;
+	let shuttingDown = false;
+
+	const output = (obj: SteppableRpcResponse) => {
+		writeRawStdout(serializeJsonLine(obj));
+	};
+
+	const success = <TCommand extends SteppableRpcCommand["type"]>(
+		id: string | undefined,
+		command: TCommand,
+		data?: object,
+	): SteppableRpcResponse => {
+		if (data === undefined) {
+			return { id, type: "response", command, success: true } as SteppableRpcResponse;
+		}
+		return { id, type: "response", command, success: true, data } as SteppableRpcResponse;
+	};
+
+	const failure = (id: string | undefined, command: string, error: unknown): SteppableRpcResponse => ({
+		id,
+		type: "response",
+		command,
+		success: false,
+		error: error instanceof Error ? error.message : String(error),
+	});
+
+	const shutdown = () => {
+		if (shuttingDown) return;
+		shuttingDown = true;
+		session.dispose();
+		killTrackedDetachedChildren();
+		process.exit(0);
+	};
+
+	process.on("SIGTERM", shutdown);
+	process.on("SIGINT", shutdown);
+
+	let commandQueue = Promise.resolve();
+	const handleCommand = async (line: string) => {
+		let command: SteppableRpcCommand;
+		try {
+			command = JSON.parse(line) as SteppableRpcCommand;
+		} catch (error) {
+			output(failure(undefined, "parse", error));
+			return;
+		}
+
+		try {
+			switch (command.type) {
+				case "restore":
+					session.restore(command.snapshot);
+					output(success(command.id, command.type, session.snapshot()));
+					break;
+
+				case "snapshot":
+					output(success(command.id, command.type, session.snapshot()));
+					break;
+
+				case "advance":
+					output(success(command.id, command.type, await session.advance(command.input)));
+					break;
+
+				case "execute_tool":
+					output(success(command.id, command.type, await session.executeTool(command.callId)));
+					break;
+
+				case "shutdown":
+					output(success(command.id, command.type));
+					shutdown();
+					break;
+			}
+		} catch (error) {
+			output(failure(command.id, command.type, error));
+		}
+	};
+
+	attachJsonlLineReader(process.stdin, (line) => {
+		commandQueue = commandQueue.then(() => handleCommand(line));
+	});
+
+	return await new Promise<never>(() => {});
+}

earendil-works/pi

packages/coding-agent/src/main.ts
@@ -47,7 +47,7 @@ import { applyHttpProxySettings, configureHttpDispatcher } from "./core/http-dis
 import { resolveCliModel, resolveModelScope, type ScopedModel } from "./core/model-resolver.ts";
 import { ModelRuntime } from "./core/model-runtime.ts";
 import { restoreStdout, takeOverStdout } from "./core/output-guard.ts";
-import { type AppMode, resolveProjectTrusted } from "./core/project-trust.ts";
+import { type AppMode as ProjectTrustAppMode, resolveProjectTrusted } from "./core/project-trust.ts";
 import type { CreateAgentSessionOptions } from "./core/sdk.ts";
 import {
 	formatMissingSessionCwdPrompt,
@@ -61,7 +61,7 @@ import { printTimings, resetTimings, time } from "./core/timings.ts";
 import { hasTrustRequiringProjectResources, ProjectTrustStore } from "./core/trust-manager.ts";
 import { builtInExtensions } from "./extensions/index.ts";
 import { runMigrations, showDeprecationWarnings } from "./migrations.ts";
-import { InteractiveMode, runPrintMode, runRpcMode } from "./modes/index.ts";
+import { InteractiveMode, runPrintMode, runRpcMode, runSteppableRpcMode } from "./modes/index.ts";
 import { initTheme, stopThemeWatcher } from "./modes/interactive/theme/theme.ts";
 import { handleConfigCommand, handlePackageCommand } from "./package-manager-cli.ts";
 import { isLocalPath, normalizePath, resolvePath } from "./utils/paths.ts";
@@ -115,9 +115,11 @@ function isTruthyEnvFlag(value: string | undefined): boolean {
 	return value === "1" || value.toLowerCase() === "true" || value.toLowerCase() === "yes";
 }
 
+type AppMode = ProjectTrustAppMode | "steppable-rpc";
+
 function resolveAppMode(parsed: Args, stdinIsTTY: boolean, stdoutIsTTY: boolean): AppMode {
-	if (parsed.mode === "rpc") {
-		return "rpc";
+	if (parsed.mode === "rpc" || parsed.mode === "steppable-rpc") {
+		return parsed.mode;
 	}
 	if (parsed.mode === "json") {
 		return "json";
@@ -128,7 +130,7 @@ function resolveAppMode(parsed: Args, stdinIsTTY: boolean, stdoutIsTTY: boolean)
 	return "interactive";
 }
 
-function toPrintOutputMode(appMode: AppMode): Exclude<Mode, "rpc"> {
+function toPrintOutputMode(appMode: AppMode): Exclude<Mode, "rpc" | "steppable-rpc"> {
 	return appMode === "json" ? "json" : "text";
 }
 
@@ -643,8 +645,8 @@ export async function main(args: string[], options?: MainOptions) {
 		takeOverStdout();
 	}
 
-	if (parsed.mode === "rpc" && parsed.fileArgs.length > 0) {
-		console.error(chalk.red("Error: @file arguments are not supported in RPC mode"));
+	if ((parsed.mode === "rpc" || parsed.mode === "steppable-rpc") && parsed.fileArgs.length > 0) {
+		console.error(chalk.red("Error: @file arguments are not supported in RPC modes"));
 		process.exit(1);
 	}
 
@@ -675,7 +677,10 @@ export async function main(args: string[], options?: MainOptions) {
 		(parsed.sessionDir ? normalizePath(parsed.sessionDir) : undefined) ??
 		(envSessionDir ? expandTildePath(envSessionDir) : undefined) ??
 		startupSettingsManager.getSessionDir();
-	let sessionManager = await createSessionManager(parsed, cwd, sessionDir, startupSettingsManager);
+	let sessionManager =
+		appMode === "steppable-rpc"
+			? SessionManager.inMemory(cwd)
+			: await createSessionManager(parsed, cwd, sessionDir, startupSettingsManager);
 	const missingSessionCwdIssue = getMissingSessionCwdIssue(sessionManager, cwd);
 	if (missingSessionCwdIssue) {
 		if (appMode === "interactive") {
@@ -705,7 +710,8 @@ export async function main(args: string[], options?: MainOptions) {
 		parsed.projectTrustOverride === undefined && !hasTrustRequiringProjectResources(sessionCwd)
 			? sessionCwd
 			: undefined;
-	const trustPromptMode: AppMode = parsed.help || parsed.listModels !== undefined ? "print" : appMode;
+	const trustPromptMode: ProjectTrustAppMode =
+		parsed.help || parsed.listModels !== undefined ? "print" : appMode === "steppable-rpc" ? "rpc" : appMode;
 	const projectTrustByCwd = new Map<string, boolean>();
 
 	const resolvedExtensionPaths = resolveCliPaths(cwd, parsed.extensions);
@@ -750,7 +756,11 @@ export async function main(args: string[], options?: MainOptions) {
 									projectTrustContext ??
 									createProjectTrustContext({
 										cwd,
-										mode: isInitialRuntime ? trustPromptMode : appMode,
+										mode: isInitialRuntime
+										? trustPromptMode
+										: appMode === "steppable-rpc"
+											? "rpc"
+											: appMode,
 										settingsManager: startupSettingsManager,
 										hasUI: isInitialRuntime && trustPromptMode === "interactive",
 									}),
@@ -865,9 +875,9 @@ export async function main(args: string[], options?: MainOptions) {
 		process.exit(0);
 	}
 
-	// Read piped stdin content (if any) - skip for RPC mode which uses stdin for JSON-RPC
+	// Read piped stdin content (if any) - skip for RPC modes which use stdin for JSON-RPC
 	let stdinContent: string | undefined;
-	if (appMode !== "rpc") {
+	if (appMode !== "rpc" && appMode !== "steppable-rpc") {
 		stdinContent = await readPipedStdin();
 		if (stdinContent !== undefined && appMode === "interactive") {
 			appMode = "print";
@@ -923,6 +933,9 @@ export async function main(args: string[], options?: MainOptions) {
 	if (appMode === "rpc") {
 		printTimings();
 		await runRpcMode(runtime);
+	} else if (appMode === "steppable-rpc") {
+		printTimings();
+		await runSteppableRpcMode(runtime);
 	} else if (appMode === "interactive") {
 		const interactiveMode = new InteractiveMode(runtime, {
 			migratedProviders,

packages/coding-agent/src/modes/steppable-rpc/steppable-rpc-mode.ts
@@ -1,0 +1,100 @@
+/**
+ * Steppable RPC mode: JSONL request/response protocol for durable drivers.
+ *
+ * Unlike normal RPC mode, this does not stream agent events on stdout. Events
+ * are returned inside advance responses so the durable driver can persist them
+ * transactionally with the snapshot and next action.
+ */
+
+import type { AgentSessionRuntime } from "../../core/agent-session-runtime.ts";
+import { takeOverStdout, writeRawStdout } from "../../core/output-guard.ts";
+import { killTrackedDetachedChildren } from "../../utils/shell.ts";
+import { attachJsonlLineReader, serializeJsonLine } from "../rpc/jsonl.ts";
+import type { SteppableRpcCommand, SteppableRpcResponse } from "./steppable-rpc-types.ts";
+
+export type { SteppableRpcCommand, SteppableRpcResponse } from "./steppable-rpc-types.ts";
+
+export async function runSteppableRpcMode(runtimeHost: AgentSessionRuntime): Promise<never> {
+	takeOverStdout();
+	const session = runtimeHost.session;
+	let shuttingDown = false;
+
+	const output = (obj: SteppableRpcResponse) => {
+		writeRawStdout(serializeJsonLine(obj));
+	};
+
+	const success = <TCommand extends SteppableRpcCommand["type"]>(
+		id: string | undefined,
+		command: TCommand,
+		data?: object,
+	): SteppableRpcResponse => {
+		if (data === undefined) {
+			return { id, type: "response", command, success: true } as SteppableRpcResponse;
+		}
+		return { id, type: "response", command, success: true, data } as SteppableRpcResponse;
+	};
+
+	const failure = (id: string | undefined, command: string, error: unknown): SteppableRpcResponse => ({
+		id,
+		type: "response",
+		command,
+		success: false,
+		error: error instanceof Error ? error.message : String(error),
+	});
+
+	const shutdown = () => {
+		if (shuttingDown) return;
+		shuttingDown = true;
+		session.dispose();
+		killTrackedDetachedChildren();
+		process.exit(0);
+	};
+
+	process.on("SIGTERM", shutdown);
+	process.on("SIGINT", shutdown);
+
+	let commandQueue = Promise.resolve();
+	const handleCommand = async (line: string) => {
+		let command: SteppableRpcCommand;
+		try {
+			command = JSON.parse(line) as SteppableRpcCommand;
+		} catch (error) {
+			output(failure(undefined, "parse", error));
+			return;
+		}
+
+		try {
+			switch (command.type) {
+				case "restore":
+					session.restore(command.snapshot);
+					output(success(command.id, command.type, session.snapshot()));
+					break;
+
+				case "snapshot":
+					output(success(command.id, command.type, session.snapshot()));
+					break;
+
+				case "advance":
+					output(success(command.id, command.type, await session.advance(command.input)));
+					break;
+
+				case "execute_tool":
+					output(success(command.id, command.type, await session.executeTool(command.callId)));
+					break;
+
+				case "shutdown":
+					output(success(command.id, command.type));
+					shutdown();
+					break;
+			}
+		} catch (error) {
+			output(failure(command.id, command.type, error));
+		}
+	};
+
+	attachJsonlLineReader(process.stdin, (line) => {
+		commandQueue = commandQueue.then(() => handleCommand(line));
+	});
+
+	return await new Promise<never>(() => {});
+}